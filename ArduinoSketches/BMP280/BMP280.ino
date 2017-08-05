ad9be123b0500fa7eb887da6d1e7f50086a13e71
amuthmann <amuthmann@pro-vision.de>
2017-08-05 14:40:19 +0200

    Distance and Pressure Sensor

---
+++ ArduinoSketches/BMP280/BMP280.ino
@@ -0,0 +1,96 @@
+/**
+ * This Sketch is based on the MySensors Pressure Sensor Sketch which uses an older version of the Sensor (BMP085).
+ * https://github.com/mysensors/MySensorsArduinoExamples/tree/master/examples/PressureSensor
+ *
+ * Original Headers:
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik Ekblad
+ * Version 1.1 - Alex Muthmann - https://dev-eth0.de
+ *
+ * DESCRIPTION
+ * Temperature and Pressure Sensor using Bosch's BMP280. It uses the Adafruit Library and provides both Temperature and current Pressure in seperate readings.
+ */
+
+
+#include <SPI.h>
+#include <MySensors.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+#define MY_RF24_CE_PIN 9
+#define MY_RF24_CS_PIN 10
+
+
+// Sleep time between reads (in milliseconds)
+const unsigned long SLEEP_TIME = 60000;
+
+
+Adafruit_BMP280 bmp;
+float lastPressure = -1;
+float lastTemp = -1;
+
+// IDs used
+#define CHILD_ID 40
+
+MyMessage tempMsg(CHILD_ID, V_TEMP);
+MyMessage pressureMsg(CHILD_ID, V_PRESSURE);
+
+
+void setup()
+{
+	if (!bmp.begin(0x76))
+	{
+		Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+		while (1) {}
+	}
+}
+
+void presentation()  {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Pressure Sensor", "1.1");
+
+  // Register sensors to gw
+  present(CHILD_ID, S_BARO);
+  present(CHILD_ID, S_TEMP);
+}
+
+void loop()
+{
+	float pressure = bmp.readPressure();
+	float temperature = bmp.readTemperature();
+
+	if (temperature != lastTemp)
+	{
+		send(tempMsg.set(temperature, V_TEMP));
+		lastTemp = temperature;
+	}
+
+	if (pressure != lastPressure)
+	{
+		send(pressureMsg.set(pressure, V_PRESSURE));
+		lastPressure = pressure;
+	}
+
+	sleep(SLEEP_TIME);
+}
+
+
---
+++ ArduinoSketches/DistanceSensor/DistanceSensor.ino
@@ -0,0 +1,89 @@
+
+/**
+ * This Sketch is based on the MySensors Distance Sensor Sketch
+ * https://github.com/mysensors/MySensorsArduinoExamples/tree/master/examples/DistanceSensor
+ *
+ * Original Headers:
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik EKblad
+ * Version 1.1 - Alex Muthmann - https://dev-eth0.de
+ *
+ * DESCRIPTION
+ * This sketch provides an example how to implement a distance sensor using HC-SR04.
+ * http://www.mysensors.org/build/distance
+ */
+
+#include <Wire.h>
+#include <SPI.h>
+#include <MySensors.h>
+#include <NewPing.h>
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+#define MY_RF24_CE_PIN 9
+#define MY_RF24_CS_PIN 10
+
+// Sleep time between reads (in milliseconds)
+unsigned long SLEEP_TIME = 3000;
+
+// Pins used for the Sensors
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+int lastDist  = -1;
+
+// ID used
+#define CHILD_ID 13
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+MyMessage msg(CHILD_ID, V_DISTANCE);
+
+
+
+void setup()
+{
+  metric = getControllerConfig().isMetric;
+}
+
+void presentation() {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Distance Sensor", "1.1");
+
+  // Register all sensors to gw
+  present(CHILD_ID, S_DISTANCE);
+}
+
+void loop()
+{
+  // Ping 5 times and calculate median
+  int dist_ms = sonar.ping_median(5);
+  // Calculate actual distance
+  int dist = sonar.convert_cm(dist_ms);
+
+  if (dist != lastDist) {
+      send(msg.set(dist));
+      lastDist = dist;
+  }
+
+  sleep(SLEEP_TIME);
+}
+
+
