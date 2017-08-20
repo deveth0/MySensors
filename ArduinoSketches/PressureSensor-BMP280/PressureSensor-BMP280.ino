e37d59a3acd28193ebf3f37e15236e2b61c4cf76
amuthmann <amuthmann@pro-vision.de>
2017-08-20 12:42:57 +0200

    Updates

--- ArduinoSketches/PressureSensor-BMP280/PressureSensor-BMP280.ino
+++ ArduinoSketches/PressureSensor-BMP280/PressureSensor-BMP280.ino
@@ -101,8 +101,8 @@ void presentation()  {
 void loop()
 {
   Serial.println("loop");
-  // Read pressure and temperature
-  float pressure = bmp.readPressure();
+  // Read pressure as mBar and temperature
+  float pressure = bmp.readPressure() / 100;
   float temperature = bmp.readTemperature();
 
   #ifdef BATTERY_SENSE_PIN
@@ -116,7 +116,7 @@ void loop()
   // Debug Output
   #ifdef MY_DEBUG
     Serial.print("Temperature = "); Serial.print(temperature); Serial.println(" *C");
-    Serial.print("Pressure = "); Serial.print(pressure); Serial.println(" hPa");
+    Serial.print("Pressure = "); Serial.print(pressure); Serial.println(" mBar");
     #ifdef BATTERY_SENSE_PIN
       Serial.print("A0 analogRead = "); Serial.println(sensorValue);
       Serial.print("Battery Voltage= "); Serial.print(volt); Serial.println(" V");
@@ -126,7 +126,7 @@ void loop()
 
   // Send values to gateway
   send(tempMsg.set(temperature, 1));
-  send(pressureMsg.set(pressure, V_PRESSURE));
+  send(pressureMsg.set(pressure, 2));
   #ifdef BATTERY_SENSE_PIN
     send(voltMsg.set(sensorValue, 0));
     sendBatteryLevel(batteryPcnt);
