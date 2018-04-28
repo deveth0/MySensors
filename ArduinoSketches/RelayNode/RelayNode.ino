0485dd144027e0eb77cb640301a1e2e9a811b1a8
Alex Muthmann <amuthmann@dev-eth0.de>
2018-04-28 18:20:06 +0200

    Add relay node

--- ArduinoSketches/RelayNode/RelayNode.ino
+++ ArduinoSketches/RelayNode/RelayNode.ino
@@ -34,34 +34,44 @@
 #include <MySensors.h>
 
 // Relay configuration
-#define RELAY_PIN 5  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 2 // Total number of attached relays
 #define RELAY_ON LOW  // GPIO value to write to turn on attached relay
 #define RELAY_OFF HIGH // GPIO value to write to turn off attached relay
 
 // Sleep time between reads (in milliseconds)
 #ifdef MY_DEBUG
-  const unsigned long SLEEP_TIME = 1000; // debug? update every half second
+  const unsigned long SLEEP_TIME = 500; // debug? update every half second
 #else
-  const unsigned long SLEEP_TIME = 1000; // once every half second
+  const unsigned long SLEEP_TIME = 500; // once every half second
 #endif
 
 
 bool initialValueSent = false;
 
 // IDs used
-#define CHILD_ID_RELAIS_1 2
-#define CHILD_ID_RELAIS_2 3
+#define CHILD_ID_RELAIS_1 0
+#define CHILD_ID_RELAIS_2 1
+#define CHILD_ID_RELAIS_3 2
+#define CHILD_ID_RELAIS_4 3
+#define PIN_RELAIS_1 5
+#define PIN_RELAIS_2 6
+#define PIN_RELAIS_3 12
+#define PIN_RELAIS_4 13
 
 MyMessage relay1Msg(CHILD_ID_RELAIS_1, V_STATUS);
 MyMessage relay2Msg(CHILD_ID_RELAIS_2, V_STATUS);
+MyMessage relay3Msg(CHILD_ID_RELAIS_3, V_STATUS);
+MyMessage relay4Msg(CHILD_ID_RELAIS_4, V_STATUS);
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("setup");
-  pinMode(RELAY_PIN, OUTPUT);
-  pinMode(RELAY_PIN + 1, OUTPUT);
+  pinMode(PIN_RELAIS_1, OUTPUT);
+  pinMode(PIN_RELAIS_2, OUTPUT);
+  pinMode(PIN_RELAIS_3, OUTPUT);
+  pinMode(PIN_RELAIS_4, OUTPUT);
+  
+  
 }
 
 void presentation()  {
@@ -71,28 +81,42 @@ void presentation()  {
   // Register relay to gw
   present(CHILD_ID_RELAIS_1, S_BINARY);
   present(CHILD_ID_RELAIS_2, S_BINARY);
+  present(CHILD_ID_RELAIS_3, S_BINARY);
+  present(CHILD_ID_RELAIS_4, S_BINARY);
   
   // Disable both relays
-  digitalWrite(RELAY_PIN, RELAY_OFF);
-  digitalWrite(RELAY_PIN + 1 , RELAY_OFF);
+  digitalWrite(PIN_RELAIS_1, RELAY_ON);
+  digitalWrite(PIN_RELAIS_2, RELAY_ON);
+  digitalWrite(PIN_RELAIS_3, RELAY_ON);
+  digitalWrite(PIN_RELAIS_4, RELAY_ON);
 
 }
 
 void loop()
-{  
+{
+  Serial.println("loop");
+  
   // send initial value for relay
+  
   if (!initialValueSent) {
     // Set state to RELAY_OFF
-    send(relay1Msg.set(RELAY_OFF));    
+    send(relay1Msg.set(RELAY_OFF));
+    send(relay2Msg.set(RELAY_OFF));
+    send(relay3Msg.set(RELAY_OFF));
+    send(relay4Msg.set(RELAY_OFF));
+    
     Serial.println("Requesting initial value from controller");
     request(CHILD_ID_RELAIS_1, V_STATUS);
     wait(2000, C_SET, V_STATUS);
-    send(relay2Msg.set(RELAY_OFF));
     request(CHILD_ID_RELAIS_2, V_STATUS);
     wait(2000, C_SET, V_STATUS);
+    request(CHILD_ID_RELAIS_3, V_STATUS);
+    wait(2000, C_SET, V_STATUS);
+    request(CHILD_ID_RELAIS_4, V_STATUS);
+    wait(2000, C_SET, V_STATUS);
   }
-  
-  delay(SLEEP_TIME);
+  Serial.println("Sleeping");
+  //sleep(SLEEP_TIME);
 }
 
 void receive(const MyMessage &message)
@@ -107,12 +131,20 @@ void receive(const MyMessage &message)
         // Change relay state
         Serial.println("Received message for "+ message.sensor);
         if(message.sensor ==  CHILD_ID_RELAIS_1){
-          digitalWrite(RELAY_PIN, message.getBool() ? RELAY_ON:RELAY_OFF);
+          digitalWrite(PIN_RELAIS_1, message.getBool() ? RELAY_ON:RELAY_OFF);
           send(relay1Msg.set(message.getBool() ? 1 : 0)); 
         }
         else if(message.sensor == CHILD_ID_RELAIS_2){
-          digitalWrite(RELAY_PIN + 1, message.getBool() ? RELAY_ON:RELAY_OFF);
+          digitalWrite(PIN_RELAIS_2, message.getBool() ? RELAY_ON:RELAY_OFF);
           send(relay2Msg.set(message.getBool() ? 1 : 0)); 
+        }
+        else if(message.sensor == CHILD_ID_RELAIS_3){
+          digitalWrite(PIN_RELAIS_3, message.getBool() ? RELAY_ON:RELAY_OFF);
+          send(relay3Msg.set(message.getBool() ? 1 : 0)); 
+        }
+        else if(message.sensor == CHILD_ID_RELAIS_4){
+          digitalWrite(PIN_RELAIS_4, message.getBool() ? RELAY_ON:RELAY_OFF);
+          send(relay4Msg.set(message.getBool() ? 1 : 0)); 
         }
     }
 }
